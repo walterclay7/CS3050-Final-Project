9bc2fe83460440639b2d524ab46ce518f162a59d
ninabraddock <113557496+ninabraddock@users.noreply.github.com>
2024-10-24 22:38:20 -0400

    forgot to add files to prev commit

---
+++ view.py
@@ -0,0 +1,187 @@
+import arcade
+import random
+# from tester import RootBeerTapper
+
+# variables for MenuView
+width = 800
+height = 600
+# SPRITE_SCALING = 0.5
+beer_speed = 7
+person_speed = 2
+bars = 4
+
+class MenuView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.WHITE)
+    # def on_show(self):
+    #     arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Tapper!", width / 2, height / 2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", width / 2, height / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        instructions_view = InstructionView()
+        self.window.show_view(instructions_view)
+
+
+class InstructionView(arcade.View):
+    # def on_show(self):
+    #     arcade.set_background_color(arcade.color.ORANGE_PEEL)
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Move using the up and down arrows.", width / 2, height / 2 + 15,
+                         arcade.color.BLACK, font_size=20, anchor_x="center", multiline=True)
+        arcade.draw_text("Use the space bar to serve the drinks", width / 2, height / 2 - 15,
+                         arcade.color.BLACK, font_size=20, anchor_x="center", multiline=True)
+        arcade.draw_text("Click to advance", width / 2, height / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = RootBeerTapper()
+        self.window.show_view(game_view)
+
+class GameOverView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+    # def on_show(self):
+    #     arcade.set_background_color(arcade.color.BLACK)
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        self.clear()
+        """
+        Draw "Game over" across the screen.
+        """
+        arcade.draw_text("Game Over", 240, 400, arcade.color.WHITE, 54)
+        arcade.draw_text("Click to restart", 310, 300, arcade.color.WHITE, 24)
+
+        output_total = f"Total Score: {self.window.total_score}"
+        arcade.draw_text(output_total, 10, 10, arcade.color.WHITE, 14)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP and self.current_bar < bars - 1:
+            self.current_bar += 1
+            self.player_sprite.center_y = self.all_bars_y[self.current_bar]
+        elif key == arcade.key.DOWN and self.current_bar > 0:
+            self.current_bar -= 1
+            self.player_sprite.center_y = self.all_bars_y[self.current_bar]
+        if key == arcade.key.SPACE:
+            beer = Beer("beer_image.png", 0.3)
+            beer.center_x = self.player_sprite.center_x + 50
+            beer.center_y = self.player_sprite.center_y
+            self.beer_list.append(beer)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        instructions_view = InstructionView()
+        self.window.show_view(instructions_view)
+
+class Beer(arcade.Sprite):
+    def update(self):
+        self.center_x += beer_speed
+        if self.right > width:
+            self.kill()
+
+
+class Customer(arcade.Sprite):
+    def update(self):
+        self.center_x -= person_speed
+        if self.left < 0:
+            self.kill()
+
+class RootBeerTapper(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.player_sprite = None
+        self.beer_list = None
+        self.customer_list = None
+        self.current_bar = 0
+        self.all_bars_y = [150, 250, 350, 450]
+        self.score = 0
+
+        self.player_sprite = arcade.SpriteSolidColor(50, 50, arcade.color.BROWN)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = self.all_bars_y[self.current_bar]
+        self.beer_list = arcade.SpriteList()
+        self.customer_list = arcade.SpriteList()
+        arcade.schedule(self.add_customer, 2)
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color_from_hex_string("#454545"))
+
+    def add_customer(self, delta_time: float):
+        customer = Customer("customer_image.png", 0.5)
+        customer.center_x = width - 50
+        customer.center_y = random.choice(self.all_bars_y)
+        self.customer_list.append(customer)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        # draw background ->bars
+        perp_rec = 10
+        bar_width = width + 10
+        bar_height = 30
+        rect_width = bar_width // perp_rec
+        shadow_height = 4
+
+        bar_start_x = width // 3 - bar_width // 2
+
+        # add background bars
+        for y_position in self.all_bars_y:
+            arcade.draw_rectangle_filled(width // 3, y_position + 25, bar_width, bar_height,
+                                         arcade.color_from_hex_string("#622A0F")) #994E31
+            # arcade.draw_rectangle_filled(width // 3, y_position + 25, bar_width, bar_height, arcade.color.from_hex_string("#994E31"))
+            arcade.draw_rectangle_filled(width // 3, y_position + 17, bar_width, shadow_height, arcade.color.BLACK)
+
+            arcade.draw_rectangle_filled(width // 3, y_position, bar_width, bar_height, arcade.color_from_hex_string("#923B1B"))
+            # perpendicular bars underneath
+
+            for i in range(perp_rec):
+                rect_x = bar_start_x + i * rect_width + rect_width // 2
+                rect_y = y_position
+
+                # actually draw them
+                arcade.draw_rectangle_filled(rect_x, rect_y, rect_width / perp_rec, bar_height, arcade.color_from_hex_string("#53220F"))
+
+        self.player_sprite.draw()
+        self.beer_list.draw()
+        self.customer_list.draw()
+        arcade.draw_text(f"Score: {self.score}", 10, height - 30, arcade.color.WHITE, 20)
+
+    def on_update(self, delta_time):
+        self.beer_list.update()
+        self.customer_list.update()
+        for beer in self.beer_list:
+            customers_hit = arcade.check_for_collision_with_list(beer, self.customer_list)
+            for customer in customers_hit:
+                beer.kill()
+                customer.kill()
+                self.score += 1
+                self.window.total_score += 1
+        # from start_and_end_screen import GameOverView   ##hella jenky idk if you can even do this but i was getting circular import errors otherwise
+        # I think its a bigger issue with the code structure but we can fix it later, this will be good to turn in for now
+        if self.score == 3:
+            game_over_view = GameOverView()
+            self.window.show_view(game_over_view)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP and self.current_bar < bars - 1:
+            self.current_bar += 1
+            self.player_sprite.center_y = self.all_bars_y[self.current_bar]
+        elif key == arcade.key.DOWN and self.current_bar > 0:
+            self.current_bar -= 1
+            self.player_sprite.center_y = self.all_bars_y[self.current_bar]
+        if key == arcade.key.SPACE:
+            beer = Beer("beer_image.png", 0.3)
+            beer.center_x = self.player_sprite.center_x + 50
+            beer.center_y = self.player_sprite.center_y
+            self.beer_list.append(beer)
